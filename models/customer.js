75cbf64ef0b7543df00b1f5c64e899ef497dea05
chady <chady@example.com>
2021-04-07 12:58:55 +0100

    created the customer endpoint

---
+++ models/customer.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+const Joi = require("joi");
+
+const customerSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 50
+  },
+  isGold: {
+    type: Boolean,
+    default: false
+  },
+  phone: {
+    type: String,
+    required: true,
+    length: 10
+  }
+});
+
+const Customer = mongoose.model("Customer", customerSchema);
+
+function validateCustomer(customer) {
+  const schema = Joi.object({
+    name: Joi.string().min(5).required(),
+    isGold: Joi.boolean(),
+    phone: Joi.string().length(10).required()
+  });
+  return schema.validate(customer);
+}
+
+exports.Customer = Customer;
+exports.validate = validateCustomer;
