5a40aac1348e7ff658c8bc9a64a62de3561e07ae
chady <chady@example.com>
2021-04-07 14:31:42 +0100

    refactored the index file

---
+++ startup/logging.js
@@ -0,0 +1,21 @@
+const winston = require("winston");
+require("winston-mongodb");
+require("express-async-errors");
+module.exports = function () {
+  winston.add(
+    new winston.transports.File({
+      filename: "logfile.log",
+      handleExceptions: true,
+      handleRejections: true
+    })
+  );
+
+  winston.add(
+    new winston.transports.MongoDB({
+      db: "mongodb://localhost/movienetic",
+      level: "error",
+      handleExceptions: true,
+      handleRejections: true
+    })
+  );
+};
